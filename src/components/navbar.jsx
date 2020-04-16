09458ce03ef18d34cdabe4198d76474dad24ebbb
sazalRahman <sazalrahman10@gmail.com>
2020-04-16 09:20:06 +0600

    Add Total count

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,9 +4,9 @@ import React from "react";
 // Stateless Function Component
 const NavBar = ({ totalCounters }) => {
 
+  console.log('totalCounters', totalCounters);
   return (
     <nav class="navbar navbar-light bg-light">
-      Navbar{" "}
       <a className="navbar-brand" href="#">
         <span className="badge badge-pill badge-secondary">{totalCounters}</span>
       </a>
